426dec7e5249ed61d672b6489c2573930665777c
Ngo Hoang <ngohoang8320@gmail.com>
2024-02-27 21:47:39 +0700

    Add practice file sql createStudentManager.sql

---
+++ sql/createStudentManager.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE QuanLySinhVien;
+USE QuanLySinhVien;
+
+CREATE TABLE Class (
+    ClassID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    ClassName VARCHAR(60) NOT NULL ,
+    StartDate DATETIME NOT NULL ,
+    Status BIT
+);
+
+CREATE TABLE Student (
+    StudentID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    StudentName VARCHAR(30) NOT NULL,
+    Address VARCHAR(50),
+    Phone VARCHAR(20),
+    Status BIT,
+    ClassID INT NOT NULL,
+    FOREIGN KEY (ClassID) REFERENCES Class(ClassID)
+);
+
+CREATE TABLE Subject (
+    SubID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    SubName VARCHAR(30) NOT NULL ,
+    Credit TINYINT NOT NULL DEFAULT 1 CHECK ( Subject.Credit >=1 ),
+    Status BIT DEFAULT 1
+);
+
+CREATE TABLE Mark (
+    MarkID INT AUTO_INCREMENT PRIMARY KEY,
+    SubID INT NOT NULL,
+    StudentID INT NOT NULL,
+    UNIQUE (SubID, StudentID),
+    Mark FLOAT DEFAULT 0 CHECK ( Mark BETWEEN 0 AND 100 ),
+    ExamTimes TINYINT DEFAULT 1,
+    FOREIGN KEY (SubID) REFERENCES Subject(SubID),
+    FOREIGN KEY (StudentID) REFERENCES Student(StudentID)
+)
