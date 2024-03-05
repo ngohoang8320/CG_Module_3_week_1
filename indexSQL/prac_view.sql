edb3ca8712f1f75d0278e7f0819da9e9539a8ffd
Ngo Hoang <ngohoang8320@gmail.com>
2024-03-05 16:46:17 +0700

    Add practice file sql prac_view

---
+++ indexSQL/prac_view.sql
@@ -0,0 +1,15 @@
+CREATE VIEW customer_views AS
+    SELECT customerNumber, customerName, phone FROM customers;
+
+SELECT * FROM customer_views;
+
+# DROP VIEW IF EXISTS customer_views;
+
+
+CREATE OR REPLACE VIEW customer_views AS
+    SELECT  customerNumber, customerName, contactFirstName, contactLastName, phone FROM customers
+    WHERE city = 'Nantes';
+
+SELECT * FROM customer_views;
+
+DROP VIEW IF EXISTS customer_views;
